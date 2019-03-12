4251bd5229117692ee59d118980747d97e442767
MohamadDani18 <mochdani2@gmail.com>
2019-03-12 23:06:09 +0700

    versi 1.0

---
+++ oop.java
@@ -0,0 +1,7 @@
+public class Mahasiswa {
+    public static void main(String[] args) {
+        System.out.println("Nama     : M Dani prayitno");
+        System.out.println("Nim      : 17090087");
+        System.out.println("Kelas    : 4C");
+    }
+}
